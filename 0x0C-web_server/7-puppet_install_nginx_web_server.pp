24199d9f4f7d7d6e167f3282b02064a0dd62c45a
Stephen Obialor <stevobialor@gmail.com>
2023-01-15 11:41:52 +0000

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,43 @@
+# install nginx and configure it
 
+package { 'nginx':
+  ensure => 'installed',
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  mode    => '0644',
+  content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                return 200
+        \"Hello World\";
+        }
+
+        location /redirect_me {
+            return 301
+        https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+	}',
+}
+
+exec { 'create_symb_link':
+  command => 'sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/',
+}
+
+exec { 'start':
+  command => 'sudo service nginx restart',
+}
