82012541fe56f20691261b8303726055b5fc1344
Stephen Obialor <stevobialor@gmail.com>
2023-01-18 20:31:58 +0000

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,11 +10,10 @@ package { 'nginx':
   provider => 'apt',
 }
 
-file_line { 'nginx.conf':
-  ensure => present,
-  path   => '/etc/nginx'
-  line   => 'http {\n\tadd_header X-Served-By $HOSTNAME;'
-  match  => 'http {'
+file_line { 'header':
+  path  => '/etc/nginx/nginx.conf',
+  line  => 'http {\n\tadd_header X-Served-By $HOSTNAME;',
+  match => 'http {',
 }
 
 exec { 'restart':
