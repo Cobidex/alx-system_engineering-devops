8a19e73ee9cfba7dc5353521af38ef0a99c0b47d
Stephen Obialor <stevobialor@gmail.com>
2023-02-28 20:37:20 +0000

    done with task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-'''
-    module for retrieving pages of listing via recursive api calls
-'''
-import requests
+
+"""
+importing requests module
+"""
+
+from requests import get
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    '''
-        method to return list of hot articles
-    '''
+    """
+    function that queries the Reddit API and returns a list containing the
+    titles of all hot articles for a given subreddit.
+    """
+
     params = {'show': 'all'}
-    header = {'User-Agent': 'Google Chrome Version 110.0.5481.177'}
-    url = 'https://www.reddit/r/{}/hot/.json?after={}'. format(subreddit,
-                                                               after)
-    if subreddit is None or type(subreddit) != str:
+
+    if subreddit is None or not isinstance(subreddit, str):
         return None
-    response = requests.get(url, header=header, params=params)
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+
+    url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit,
+                                                                  after)
+
+    response = get(url, headers=user_agent, params=params)
+
     if (response.status_code != 200):
         return None
-    data = response.json()
-    after = data.get('data').get('after')
-    if after:
-        titles = data.get('data').get('children')
-        for t in titles:
-            title = t.get('data').get('title')
-            hot_list.append(title)
+
+    all_data = response.json()
+
+    try:
+        raw1 = all_data.get('data').get('children')
+        after = all_data.get('data').get('after')
+
+        if after is None:
+            return hot_list
+
+        for i in raw1:
+            hot_list.append(i.get('data').get('title'))
+
         return recurse(subreddit, hot_list, after)
-    return hot_list
+    except:
+        print("None")
