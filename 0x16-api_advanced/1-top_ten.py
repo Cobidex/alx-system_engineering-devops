aa8081c0d5f2a42bf5f34321d4aeb0f0b4e30b03
Stephen Obialor <stevobialor@gmail.com>
2023-02-28 14:20:29 +0000

    print title of movie

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Importing requests module
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints the titles of the first 10 hot posts listed for a given subreddit..
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print('None')
+
+    user_agent = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
+    para = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'. format(subreddit)
+    response = requests.get(url, headers=user_agent, params=para)
+    a_data = response.json()
+    li = a_data.get('data').get('children')
+    for ti in li:
+        title = ti.get('data').get('title')
+        print(title)
