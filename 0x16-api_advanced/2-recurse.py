5e69e2b8896fe95e2dd672dec8ec815ff2426fc5
Stephen Obialor <stevobialor@gmail.com>
2023-02-28 20:41:27 +0000

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -15,7 +15,7 @@ def recurse(subreddit, hot_list=[], after=None):
 
     params = {'show': 'all'}
 
-    if subreddit is None or not isinstance(subreddit, str):
+    if subreddit is None or not type(subreddit) != str:
         return None
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
@@ -30,16 +30,13 @@ def recurse(subreddit, hot_list=[], after=None):
 
     all_data = response.json()
 
-    try:
-        raw1 = all_data.get('data').get('children')
-        after = all_data.get('data').get('after')
+    raw = all_data.get('data').get('children')
+    after = all_data.get('data').get('after')
 
-        if after is None:
-            return hot_list
+    if after is None:
+        return hot_list
 
-        for i in raw1:
-            hot_list.append(i.get('data').get('title'))
+    for i in raw:
+        hot_list.append(i.get('data').get('title'))
 
-        return recurse(subreddit, hot_list, after)
-    except:
-        print("None")
+    return recurse(subreddit, hot_list, after)
